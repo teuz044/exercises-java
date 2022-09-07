db23a76fac617486d65bc01b4ba7ed2c2ffd9ad0
Matheus Gabriel <103612213+teuz044@users.noreply.github.com>
2022-09-06 23:47:31 -0300

    Delete ContaBancaria/doc directory

--- ContaBancaria/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"br.com.contabancaria","l":"ContasBancarias"},{"p":"br.com.contabancaria","l":"main"}];updateSearchResults();
